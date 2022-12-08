12a4a6b167364a511e13c7c17e0ac8620a7949d9
Faith Deanon <afdeanon@gmail.com>
2022-12-07 20:05:37 -0800

    User can see list of cafes stored in firestore

---
+++ lib/widgets/restaurantList.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:rank133/Colors/appColors.dart';
+
+class RestrauntList extends StatefulWidget {
+  static const routeName = '/home';
+  const RestrauntList({Key? key, User? user}) : super(key: key);
+  
+  @override
+  _RestrauntListState createState() => _RestrauntListState();
+
+}
+
+class _RestrauntListState extends State<RestrauntList> {
+  final CollectionReference _cafes = FirebaseFirestore.instance.collection('CafeName');
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      body: StreamBuilder(
+        // backgroundColor: screenBackgroundColor,
+        // child: _widgetOptions.elementAt(_selectedIndex),
+        stream: _cafes.snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot){
+          if(streamSnapshot.hasData){
+            return ListView.builder(
+              itemCount: streamSnapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                final DocumentSnapshot documentSnapshot = streamSnapshot.data!.docs[index];
+                return Card(
+                  margin: const EdgeInsets.all(5),
+                  child: ListTile(
+                    leading: Image.network(documentSnapshot["Images"][0]),
+                    title: Text(documentSnapshot["Name"]),
+                    subtitle: Text(documentSnapshot["Address"]),
+                  ),
+                );
+              }
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
+
